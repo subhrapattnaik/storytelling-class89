d48dbe340c9e267c5ab1b883d07a08ea24931beb
Subhra Pattnaik <50340351+subhrapattnaik@users.noreply.github.com>
2022-06-19 00:42:24 +0530

    Add files via upload

---
+++ storytelling-subhra-class89-everythingworks-likeaction-storyscreentoo/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAZn-aCAxKKHJLSl2QhyLxp8zqhTPQ3j7g",
+  authDomain: "stapp-1d102.firebaseapp.com",
+  databaseURL: "https://stapp-1d102-default-rtdb.firebaseio.com",
+  projectId: "stapp-1d102",
+  storageBucket: "stapp-1d102.appspot.com",
+  messagingSenderId: "664150369794",
+  appId: "1:664150369794:web:587cb9044462538af7691b"
+}
